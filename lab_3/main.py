a296320eaa8054cba4770e6e5bd882ab03133d3a
darikmudarik <darikmudarik@gmail.com>
2019-12-11 18:00:44 +0300

    cool code

--- lab_3/main.py
+++ lab_3/main.py
@@ -3,7 +3,7 @@ Labour work #3
  Building an own N-gram model
 """
 
-from math import log
+import math
 
 REFERENCE_TEXT = ''
 if __name__ == '__main__':
@@ -33,9 +33,9 @@ class WordStorage:
                 if value == id:
                     return key
         else:
-            return str(None)
+            return 'UNK'
         if id not in self.storage.values():
-            return str(None)
+            return 'UNK'
 
     def from_corpus(self, corpus: tuple):
         if type(corpus) == tuple:
@@ -65,30 +65,37 @@ class NGramTrie:
         return 'OK'
 
     def calculate_log_probabilities(self):
-        for n_gram in self.gram_frequencies:
-            finded_part = n_gram[:self.size - 1]
-            n_gram_frequency = self.gram_frequencies[n_gram]
-            n_grams_frequency = 0
-            for n_grams in self.gram_frequencies:
-                compared_part = n_grams[:self.size - 1]
-                if compared_part == finded_part:
-                    n_grams_frequency += self.gram_frequencies[n_grams]
-            probability = n_gram_frequency / n_grams_frequency
-            self.gram_log_probabilities[n_gram] = log(probability)
+        for gram in self.gram_frequencies:
+            sum_grams = 0
+            for gram_i in self.gram_frequencies:
+                if gram[:-1] == gram_i[:-1]:
+                    sum_grams += self.gram_frequencies[gram_i]
+            self.gram_log_probabilities[gram] = math.log(self.gram_frequencies[gram] / sum_grams)
+        return self.gram_log_probabilities
 
     def predict_next_sentence(self, prefix: tuple) -> list:
-        sentence = []
-        if type(prefix) == tuple and len(prefix) == self.size + 1 and prefix:
-            sentence.extend(prefix)
-            n_grams = list(self.gram_log_probabilities.keys())
-            prefixes = [gram[:-1] for gram in n_grams]
-            while prefix in prefixes:
-                with_prefix = [gram for gram in n_grams if gram[:-1] == prefix]
-                values = [value for key, value in self.gram_log_probabilities.items() if key in with_prefix]
-                ind = values.index(max(values))
-                sentence.append(with_prefix[ind][-1])
-                prefix = tuple(sentence[-(self.size - 1):])
-        return sentence
+        future_word = []
+        if not isinstance(prefix, tuple) or len(prefix) != self.size - 1:
+            return []
+        predicted_sentence = list(prefix)
+        flag = True
+        while flag:
+            probabilities_list = []
+            for gram in list(self.gram_log_probabilities.keys()):
+                if gram[:-1] == prefix:
+                    probabilities_list.append(self.gram_log_probabilities[gram])
+            if not probabilities_list:
+                break
+            probabilities_list.sort(reverse=True)
+            big_probability = probabilities_list[0]
+            for gram, probability in list(self.gram_log_probabilities.items()):
+                if big_probability == probability:
+                    future_word = gram[-1]
+            predicted_sentence.append(future_word)
+            new_prefix = list(prefix[1:])
+            new_prefix.append(future_word)
+            prefix = tuple(new_prefix)
+        return predicted_sentence
 
 def encode(storage_instance, corpus) -> list:
     code_list = []
@@ -104,8 +111,8 @@ def split_by_sentence(text: str) -> list:
     if type(text) != str or text is None or ' ' not in text:
         return []
     for element in text:
-        if not element.isalpha() and element not in '!?.':
-            text = text.replace(element, ' ')
+        if not element.isalpha() and element not in '!?. ':
+            text = text.replace(element, '')
     for element in text:
         if element in '!?':
             text = text.replace(element, '.').lower()
@@ -116,10 +123,32 @@ def split_by_sentence(text: str) -> list:
         new_sentence = ['<s>']
         sentence = element.split()
         for word in sentence:
-            if word != sentence or word != '':
+            if word != sentence:
                 new_sentence.append(word)
         new_sentence.append('</s>')
         new_sentences.append(new_sentence)
+        if ['<s>', '</s>'] in new_sentences:
+            new_sentences.remove(['<s>', '</s>'])
 
 
     return new_sentences
+
+def start(REFERENCE_TEXT, n, prefix):
+    word_storage = WordStorage()
+    n_gram_trie = NGramTrie(n)
+    corpus = split_by_sentence(REFERENCE_TEXT)
+    for element in corpus:
+        word_storage.from_corpus(tuple(element))
+    encoded_corpus = encode(word_storage, corpus)
+    for sentence in encoded_corpus:
+        n_gram_trie.fill_from_sentence(tuple(sentence))
+    n_gram_trie.calculate_log_probabilities()
+    prefix_new = []
+    for el in prefix:
+        prefix_new.append(word_storage.get_id_of(el))
+    final_sentence = n_gram_trie.predict_next_sentence(tuple(prefix_new))
+    return(final_sentence)
+
+test_text = 'Mar#y wa$nted, to swim! However, she was afraid of sharks.'
+print(split_by_sentence(test_text))
+print(start(REFERENCE_TEXT, 2, ('this',)))
